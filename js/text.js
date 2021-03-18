09cd9da2770067c6d685074091c22dec84c91974
Bright Akowuah <akowuahbright1@gmail.com>
2021-03-18 00:09:59 +0000

    changes made, search and other html pages added

--- js/text.js
+++ js/text.js
@@ -1,10 +1,28 @@
-var btn = document.getElementById("btn");
+// Get the modal
+var modal = document.getElementById('myModal');
 
-function alertUserName(){
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-var userEmail= document.getElementById("userEmail").value;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("search")[0];
 
-alert(userEmail);
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
 
-};
-btn.addEventListener("click",alertUserName);
+/*
+ When the user clicks on <span> (search icon), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+   
+}
+*/
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
